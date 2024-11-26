1a0770b519a9d09bf07f873ff196f3e48f583331
PanyaHantula <panya_hantula@hotmail.co.th>
2024-11-26 17:01:49 +0700

    Recevie set GPS data.

--- GPS_JamDetection.ino
+++ GPS_JamDetection.ino
@@ -3,21 +3,23 @@
 #define RXD 22
 #define TXD 21
 
-HardwareSerial GPS(2);  // Using HardwareSerial1 for GPS communication (GPIO16 RX, GPIO17 TX)
+HardwareSerial GPS(2); // Using HardwareSerial1 for GPS communication (GPIO16 RX, GPIO17 TX)
 
 // Buffer for the incoming GPS NMEA sentence
 String gpsData = "";
-int Recive_GPS_Name[20];
-int Recive_GPS_SNR[20];
+int Receive_GPS_Name[20];
+int Receive_GPS_SNR[20];
 int SatellitesIndex = 0;
 
+int CounterGPSRecevie = 0;
 
-void setup() {
+void setup()
+{
   // Initialize Serial Monitor for debugging
   Serial.begin(115200);
 
   // Initialize HardwareSerial for GPS communication (9600 baud rate)
-  GPS.begin(9600, SERIAL_8N1, RXD, TXD);  // Pin 16 RX, Pin 17 TX for example
+  GPS.begin(9600, SERIAL_8N1, RXD, TXD); // Pin 16 RX, Pin 17 TX for example
 
   Serial.println("GPS Start");
 }
@@ -32,7 +34,7 @@ void loop() {
     if (c == '\n') {
       gpsData.trim();  // Remove any leading/trailing spaces or newlines
       if (gpsData.startsWith("$GPGSV")) {
-        Serial.println(gpsData);
+        //Serial.println(gpsData);
 
         // split data with ","
         int numFields = 0;
@@ -53,15 +55,6 @@ void loop() {
         int messageNum = GPS_fields[2].toInt();       // Message number in this sentence
         int totalSatellites = GPS_fields[3].toInt();  // Total number of satellites in view
 
-        // Print basic info about the GPGSV sentence
-        //Serial.print("Total Satellites in View: ");
-        //Serial.println(totalSatellites);
-        //Serial.print("Message Count: ");
-        //Serial.println(messageCount);
-        //Serial.print("Message Number: ");
-        //Serial.println(messageNum);
-        //Serial.println("--------------------------");
-
         // -----------------------------------------------------------------------------------------------------
         // Extract and display satellite data (4 fields per satellite)
         int numberOfSatellites = (numFields - 4) / 4;  // Each satellite data takes 4 fields
@@ -69,43 +62,37 @@ void loop() {
           int prn = GPS_fields[4 + i * 4].toInt();  // PRN (Satellite ID)
           int snr = GPS_fields[7 + i * 4].toInt();  // Signal-to-noise ratio (dB)
 
-          // Print satellite data
-          //Serial.print("Satellite PRN: ");
-          //Serial.print(prn);
-          //Serial.print("\tSNR: ");
-          //Serial.println(snr);
-
-          Recive_GPS_Name[SatellitesIndex] = prn;
-          Recive_GPS_SNR[SatellitesIndex] = snr;
+          Receive_GPS_Name[SatellitesIndex] = prn;
+          Receive_GPS_SNR[SatellitesIndex] = snr;
           SatellitesIndex = SatellitesIndex + 1;
         }
 
-
         if (messageCount == messageNum) {
-          Serial.println();
-          Serial.print("Total Satellites in View: ");
-          Serial.println(totalSatellites);
-
-          Serial.print("PRN:\t");
+          //Serial.println();
+          //Serial.print("Total Satellites in View: ");
+          //Serial.println(totalSatellites);
+          Serial.print(CounterGPSRecevie);
+          Serial.print("\tPRN\t");
           for (int i = 0; i < SatellitesIndex; i++) {
-            Serial.print(Recive_GPS_Name[i]);
+            Serial.print(Receive_GPS_Name[i]);
             Serial.print("\t");
           }
           Serial.println();
 
-          Serial.print("SNR:\t");
+          Serial.print(CounterGPSRecevie);
+          Serial.print("\tSNR\t");
           for (int i = 0; i < SatellitesIndex; i++) {
-            Serial.print(Recive_GPS_SNR[i]);
+            Serial.print(Receive_GPS_SNR[i]);
             Serial.print("\t");
           }
-          Serial.println();
-          Serial.println("----------------------------------------------------------------------------------------------");
           Serial.println();
 
           SatellitesIndex = 0;
+          CounterGPSRecevie++;
         }
       }
       gpsData = "";  // Reset gpsData for the next sentence
     }
   }
+  
 }
